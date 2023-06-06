ee6ac2ad9a9966e8e3c554d5ee8daf1107e69360
Sven Schneider <sven.schneider@dvag.com>
2023-06-06 12:11:22 +0200

    CHANGE use config.json instead of slices

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 
@@ -10,53 +12,55 @@ import (
 
 const DEBUG = false
 
+// Define the struct to hold the JSON data
+type FileTypes struct {
+	Filetypes []FileType `json:"filetypes"`
+}
 type FileType struct {
-	Name      string
-	Extension string
-	Category  string
+	Name      string `json:"name"`
+	Extension string `json:"extension"`
+	Category  string `json:"category"`
 }
 
 func main() {
 	userDir := getUserHomeDir()
 	df := userDir + "/Downloads/"
 
-	cat := []string{"Documents", "Executables", "Images",
-		"Fonts", "Sounds", "Misc", "Compressed", "Videos"}
-
-	fileTypes := []FileType{
-		{Name: "PDF", Extension: ".pdf", Category: cat[0]},
-		{Name: "EPub", Extension: ".pub", Category: cat[0]},
-		{Name: "Text", Extension: ".txt", Category: cat[0]},
-		{Name: "Word", Extension: ".docx", Category: cat[0]},
-		{Name: "Exel", Extension: ".xlsx", Category: cat[0]},
-		{Name: "Markdown", Extension: ".md", Category: cat[0]},
-		{Name: "Powerpoint", Extension: ".pptx", Category: cat[0]},
-		{Name: "Program", Extension: ".exe", Category: cat[1]},
-		{Name: "Setup", Extension: ".msi", Category: cat[1]},
-		{Name: "ZIP", Extension: ".zip", Category: cat[6]},
-		{Name: "RAR", Extension: ".rar", Category: cat[6]},
-		{Name: "PNG", Extension: ".png", Category: cat[2]},
-		{Name: "JPEG", Extension: ".jpg", Category: cat[2]},
-		{Name: "BMP", Extension: ".bmp", Category: cat[2]},
-		{Name: "TIFF", Extension: ".tiff", Category: cat[2]},
-		{Name: "GIF", Extension: ".gif", Category: cat[2]},
-		{Name: "MP3", Extension: ".mp3", Category: cat[4]},
-		{Name: "WAVE", Extension: ".wav", Category: cat[4]},
-		{Name: "MP4", Extension: ".mp4", Category: cat[4]},
-		{Name: "Playlist", Extension: ".pls", Category: cat[4]},
-		{Name: "OGG", Extension: ".ogg", Category: cat[4]},
-		{Name: "MKV", Extension: ".mkv", Category: cat[7]},
-		{Name: "AVI", Extension: ".avi", Category: cat[7]},
-		{Name: "MPG", Extension: ".mpg", Category: cat[7]},
-		{Name: "Font", Extension: ".ttf", Category: cat[3]},
+	// Open the file
+	file, err := os.Open("config.json")
+	if err != nil {
+		log.Fatalf("ERROR: can't open config.json: %s\n", err)
 	}
+	defer file.Close()
+
+	// Read the file contents
+	fileContents, err := os.ReadFile("config.json")
+	if err != nil {
+		log.Fatalf("ERROR: can't read config.json: %s\n", err)
+	}
+
+	var fileData FileTypes
+	if err := json.Unmarshal(fileContents, &fileData); err != nil {
+		log.Fatalf("ERROR: can't decode config.json: %s\n", err)
+	}
+
+	// Access the decoded file types
+	fileTypes := fileData.Filetypes
+
+	// Print the decoded data
+	// for _, fileType := range fileTypes {
+	// 	fmt.Printf("Name: %s\nExtension: %s\nCategory: %s\n\n", fileType.Name, fileType.Extension, fileType.Category)
+	// }
 
 	fmt.Println("Watch Dawg started...")
 
 	fmt.Println("\nChecking category folders...")
-	for _, c := range cat {
-		checkFolder(df, c)
+
+	// create category folders if they don't exist
+	for _, fileType := range fileTypes {
+		checkFolder(df, fileType.Category)
 	}
+
 	fmt.Printf("\nDone!\n\n")
 	fmt.Printf("Observing download folder: %s\n", df)
 
